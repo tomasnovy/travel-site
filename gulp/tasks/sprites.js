0f1d91f7fa385c547182aa1f4f0b222ca8c85ca8
Tomas <tomasnovy1@gmail.com>
2017-12-29 00:34:57 +0100

    Added few changes in gulp

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,6 +5,7 @@ rename = require('gulp-rename');
 var config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -20,10 +21,15 @@ gulp.task('createSprite', function() {
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
 gulp.task('copySpriteCSS', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteCSS']);
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
