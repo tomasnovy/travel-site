9b754835c9b96b4dfceb6de2477aa5afe0454b78
Tomas <tomasnovy1@gmail.com>
2018-01-08 02:16:24 +0100

    Completed lazy loading of images.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,13 +4,21 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
+    this.lazyImages = $(".lazyload");
     this.siteHeader = $(".site-header");
     this.headerTriggerElement = $(".large-hero__title");
     this.createHeaderWaypoint();
     this.pageSections = $(".page-section");
     this.headerLinks = $(".primary-nav a");
     this.createPageSectionWaypoints();
-    this.addSmoothScrolling()
+    this.addSmoothScrolling();
+    this.refreshWaypoints();
+  }
+  
+  refreshWaypoints() {
+    this.lazyImages.on('load', function() {
+      Waypoint.refreshAll();
+    });
   }
   
   addSmoothScrolling() {
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,3 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
-
-
-
-
